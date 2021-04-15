6d9f9e2de0ab8704a91eeb61b1f431a3c19108ab
yangyipeng <yangyipeng@star-net.cn>
2021-04-15 13:36:16 +0800

    283-移动零

--- 2021/4/66-加一.js
+++ 2021/4/66-加一.js
@@ -54,3 +54,20 @@ const plusOne = function (digits) {
 
 
 console.log(plusOne([9,9]))
+
+
+
+/**
+const plusOne = function (digits) {
+    for(let i=digits.length-1;i>=0;i--){
+        digits[i]=digits[i]+1
+        digits[i]=digits[i]%10
+        if(digits[i]!==0){
+            return digits
+        }
+    }
+    digits.unshift(1)
+    return digits
+};
+*/
+
