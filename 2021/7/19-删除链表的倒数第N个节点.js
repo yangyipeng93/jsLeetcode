0d672ac275e739ca1126fbeb8c519947be8a314c
yangyipeng <yangyipeng@star-net.cn>
2021-07-06 18:03:51 +0800

    19-删除链表的倒数第N个节点.js

---
+++ 2021/7/19-删除链表的倒数第N个节点.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+const removeNthFromEnd = function (head, n) {
+
+    while (!head.next){
+
+    }
+
+}
