53af0ae10d148f04f0a10fadee953e0183127c76
yangyipeng <yangyipeng@star-net.cn>
2021-07-12 09:54:09 +0800

    19-删除链表的倒数第N个节点.js

--- 2021/7/19-删除链表的倒数第N个节点.js
+++ 2021/7/19-删除链表的倒数第N个节点.js
@@ -1,3 +1,6 @@
+
+
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -5,8 +8,6 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
-
-
 /**
  * @param {ListNode} head
  * @param {number} n
@@ -14,26 +15,40 @@
  */
 const removeNthFromEnd = function (head, n) {
 
-  let linkLength = getLength();
-  // const dummyNode={
-  //   next:head,
-  //   val:0,
-  // };
-  if (linkLength <= 1) {
-    head = null;
+  let size = 0;
+  let current = head;
+
+  while (current) {
+    current = current.next;
+    size++;
   }
 
-  let currentNode=head;
+  //虚拟节点 减少头部判断
+  const dummyNode={
+    next:head,
+    val:0,
+  };
+
+  size++
+  // if (size <= 1) {
+  //   head = null;
+  //   return head;
+  // }
+  //
+  // if (size===n){
+  //   return head.next;
+  // }
+
+  let currentNode=dummyNode;
   let count=1;
-  while (count>linkLength-n){
+  while (count<size-n){
     currentNode=currentNode.next;
     count++
   }
 
   currentNode.next=currentNode.next.next;
 
-  return head;
-
+  return dummyNode.next;
 }
 
 
@@ -51,3 +66,23 @@ const getLength = (head) => {
 }
 
 
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+
+// const five=new ListNode(5,null);
+// const four=new ListNode(4,five);
+// const three=new ListNode(3,four);
+const two=new ListNode(2,null);
+const head=new ListNode(1,two);
+
+// [1,2,3,4,5]
+// 2
+removeNthFromEnd(head,2);
+
+let current=head;
+while (current){
+  console.log(current.val);
+  current=current.next;
+}
