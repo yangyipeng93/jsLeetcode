abde20ecd009afee39eb5256241c169f380761a6
yangyipeng <yangyipeng@star-net.cn>
2021-07-30 14:00:29 +0800

    101-对称二叉树.js

--- 2021/7/101-对称二叉树.js
+++ 2021/7/101-对称二叉树.js
@@ -4,8 +4,8 @@
 
  例如，二叉树[1,2,2,3,4,4,3] 是对称的。
 
-   1
-  / \
+ 1
+ / \
  2   2
  / \ / \
  3  4 4  3
@@ -13,13 +13,12 @@
 
  但是下面这个[1,2,2,null,3,null,3] 则不是镜像对称的:
 
-  1
+ 1
  / \
  2   2
  \   \
  3    3
  */
-
 
 
 /**
@@ -34,30 +33,33 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-const isSymmetric = function(root) {
+const isSymmetric = function (root) {
 
-  if (!root) {
+  if (root == null) {
     return true;
   }
-
-  let leftRoot=root.left;
-  let rightRoot=root.right;
-  let leftArr=[];
-  let rightArr=[];
-
-  let leftStack=[leftRoot];
-  let rightQueue=[rightRoot];
-
-  while (leftRoot!=null||leftStack.length>0){
-     while (leftRoot.left){
-       leftStack.push(leftRoot);
-     }
-     let leftRoot=leftStack.pop();
-
-  }
-
+  //调用递归函数，比较左节点，右节点
+  return dfs(root.left, root.right);
 
 };
+
+function dfs(left, right) {
+  //递归的终止条件是两个节点都为空
+  //或者两个节点中有一个为空
+  //或者两个节点的值不相等
+  if (left == null && right == null) {
+    return true;
+  }
+  if (left == null || right == null) {
+    return false;
+  }
+  if (left.val !== right.val) {
+    return false;
+  }
+  //再递归的比较 左节点的左孩子 和 右节点的右孩子
+  //以及比较  左节点的右孩子 和 右节点的左孩子
+  return dfs(left.left, right.right) && dfs(left.right, right.left);
+}
 
 
 function TreeNode(val, left, right) {
@@ -70,7 +72,7 @@ function TreeNode(val, left, right) {
 
 const left15 = new TreeNode(3);
 const right7 = new TreeNode(3);
-const left9 = new TreeNode(2,null,right7);
+const left9 = new TreeNode(2, null, right7);
 const right20 = new TreeNode(2, null, left15);
 
 const root = new TreeNode(1, left9, right20);
